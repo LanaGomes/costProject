1ec91514fb2f682bac3c9c3c8a2a7e94f84680f1
Lana Gomes <lana.gomes1919@gmail.com>
2023-11-20 19:43:18 -0300

    updated date and year

--- src/components/Month.jsx
+++ src/components/Month.jsx
@@ -1,12 +1,43 @@
 import setaEsquerda from "../images/setaEsquerda.png";
 import setaDireita from "../images/setaDireita.png";
+import moment from "moment";
+import { useState } from "react";
 
 function Month() {
+  const [currentMonth, setCurrentMonth] = useState(moment());
+
+  const handleMonthChange = (operation) => {
+    setCurrentMonth((prevMonth) =>
+      operation === "next"
+        ? prevMonth.clone().add(1, "month")
+        : prevMonth.clone().subtract(1, "month")
+    );
+  };
+
+  const formattedMonth = currentMonth.format("MMMM YYYY");
+
   return (
-    <section className="monthSection">
-      <img src={setaEsquerda} alt="iconeSetaParaEsquerda"></img>
-      <h1>Month</h1>
-      <img src={setaDireita} alt="iconeSetaParadireita"></img>
+    <section
+      className="monthSection"
+      onClick={(e) => {
+        if (e.target.className.includes("setaEsquerda")) {
+          handleMonthChange("previous");
+        } else if (e.target.className.includes("setaDireita")) {
+          handleMonthChange("next");
+        }
+      }}
+    >
+      <img
+        className="setaEsquerda"
+        src={setaEsquerda}
+        alt="iconeSetaParaEsquerda"
+      ></img>
+      <h1>{formattedMonth}</h1>
+      <img
+        className="setaDireita"
+        src={setaDireita}
+        alt="iconeSetaParadireita"
+      ></img>
     </section>
   );
 }
