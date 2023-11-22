a389b9e6dab681caa8e73c72ff00aaa99ea738e5
Lana Gomes <lana.gomes1919@gmail.com>
2023-11-22 17:02:59 -0300

    dinamic date updated

--- src/components/SaldoMensal.jsx
+++ src/components/SaldoMensal.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import Saldo from "./Saldo";
+import moment from "moment";
 
 function SaldoMensal() {
+  const dateSaldo = moment().format("L");
+
   return (
     <main className="saldoContainer">
       <div className="saldoContainerSubDiv">
         <Saldo />
-        <p>08/11/2023</p> {/*atualizar data moment*/}
+        <p>{dateSaldo}</p>
       </div>
       <Link to={"/extrato"}>
         <h3 className="layoutLink">Extrato</h3>
